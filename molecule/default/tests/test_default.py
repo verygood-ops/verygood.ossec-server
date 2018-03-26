98121a101fd6b7bafcb65bc8b9a4de1254821798
Andrey Khon <akhon@samodelkin.net>
2018-03-27 00:52:22 +0700

    added socket tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -3,20 +3,30 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+  os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
 def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+  f = host.file('/etc/hosts')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+  assert f.exists
+  assert f.user == 'root'
+  assert f.group == 'root'
 
-#
-# import os
-# import testinfra.utils.ansible_runner
-# testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-#     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-# def test_sos_report_package(host):
-#     assert host.package("ossec-wazuh").is_installed
+
+def test_ossec_package(host):
+  ossec = host.package("ossec-wazuh")
+  assert ossec.is_installed
+
+
+def test_ossec_running_and_enabled(host):
+  ossec = host.service("wazuh-manager")
+  assert ossec.is_running
+  assert ossec.is_enabled
+
+
+def test_ossec_ports_opened(host):
+  ossec_tcp = host.socket("tcp://0.0.0.0:1515")
+  ossec_udp = host.socket("udp://0.0.0.0:1514")
+  assert ossec_tcp.is_listening
+  assert ossec_udp.is_listening
