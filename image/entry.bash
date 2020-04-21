4773289558c4dec4cf7c5c86ec5cfa1ce78b740e
mixua <oleksandr.medvid@vgs.io>
2020-04-22 00:50:32 +0300

    changed the proto for server and sec clients

--- image/entry.bash
+++ image/entry.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 service wazuh-manager start
-ln -sf /dev/stdout /var/ossec/logs/alerts/alerts.json
+echo "Starting push logs to stdout"
+tail --pid $$ -F /var/ossec/logs/alerts/alerts.json; wait
+# ln -sf /dev/stdout /var/ossec/logs/alerts/alerts.json
--- image/image_build.sh
+++ image/image_build.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
 set -e
 
-IMG_TAG=${IMG_TAG:-"0.0.1"}
+IMG_TAG=${IMG_TAG:-"0.0.3"}
 SERVICE=$(grep -A1 "services:" docker-compose.yaml|tail -n1|awk '{print $1}'|cut -d":" -f1)
 
 rm -rf verygood.ossec-server
 git clone git@github.com:verygood-ops/verygood.ossec-server.git verygood.ossec-server
 cp -f ../templates/vgs_rules.xml.j2 ./
 
+function publish() {
+  if [ $stat -eq 0 ]; then
+    echo -e "\nAttempt to publish an image\n"
+    IMG_TAG=$IMG_TAG docker-compose push $SERVICE
+  fi
+}
+
 if [[ -n $BUILD ]]
 then
-    IMG_TAG=$IMG_TAG docker-compose -f docker-compose.yaml up --build
+    IMG_TAG=$IMG_TAG docker-compose -f docker-compose.yaml build
     stat=$?
+    publish
   else
     IMG_TAG=$IMG_TAG docker-compose -f docker-compose.yaml up
-    stat=$?
 fi
 
 rm -rf verygood.ossec-server
 
-if [ $stat -eq 0 ]; then
-    echo -e "\nAttempt to publish an image\n"
-    IMG_TAG=$IMG_TAG docker-compose push $SERVICE
-fi
