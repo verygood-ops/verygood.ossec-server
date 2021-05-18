ef1a95e5a8f448d8c84a849c9e042c52ea410e7a
Oleksandr Medvid <oleksandr.medvid@vgs.io>
2021-05-18 13:20:06 +0300

    feature: added some impovments to container build

--- image/image_build.sh
+++ image/image_build.sh
@@ -1,29 +1,41 @@
 #!/usr/bin/env bash
 set -e
 
+# This script will help to build test/run localy and/or publish ossec-server image to ECR or QUAY regs.
+# To login to ECR Registry 'aws --region=us-west-2 ecr get-login --no-include-email'
+# To publish to quay.io 'BUILD=1 REG=1 bash image_build.sh'
+
 IMG_TAG=${IMG_TAG:-"0.0.7"}
+REPO_NAME=ossec-server
 REGION=${AWS_DEFAULT_REGION:-us-west-2}
 ACC_ID=$(aws sts get-caller-identity --query Account --output text)
 SERVICE=$(grep -A1 "services:" docker-compose.yaml|tail -n1|awk '{print $1}'|cut -d":" -f1)
 
-[[ -n $BUILD ]] && echo "Publishing container...\n" || echo "Starting container...\n, run 'BUILD=1 bash $0' in case wanted to build & publish"
-[[ -n $REG ]] && REGISTRY="quay.io/verygoodsecurity/ossec-server" || REGISTRY="${ACC_ID}.dkr.ecr.${REGION}.amazonaws.com/vgs/ossec-server" 
+function check_repo_exists(){
+  echo "Check the repo vgs/${REPO_NAME} exists if not create..."
+  local CMD="aws --region=$REGION ecr"
+  $CMD describe-repositories --repository-names vgs/${REPO_NAME} >/dev/null || \
+  $CMD create-repository --repository-name vgs/${REPO_NAME}  
+}
 
+[[ -n $BUILD ]] && echo -e "\n Publishing container...\n" || \
+                   echo -e "\n Starting container...\n, run 'BUILD=1 bash $0' in case wanted to build & publish"
+[[ -n $REG ]] && export REGISTRY="quay.io/verygoodsecurity/${REPO_NAME}" || \
+                 export REGISTRY="${ACC_ID}.dkr.ecr.${REGION}.amazonaws.com/vgs/${REPO_NAME}"; check_repo_exists
 
 rm -rf verygood.ossec-server
 git clone git@github.com:verygood-ops/verygood.ossec-server.git verygood.ossec-server
 
 function publish() {
   if [ $stat -eq 0 ]; then
-    echo -e "\nAttempt to publish an image\n"
+    echo -e "\n Attempt to publish an image...\n"
     IMG_TAG=$IMG_TAG REGISTRY=$REGISTRY docker-compose push $SERVICE
   fi
 }
 
 if [[ -n $BUILD ]]; then
     IMG_TAG=$IMG_TAG REGISTRY=$REGISTRY docker-compose -f docker-compose.yaml build
-    stat=$?
-    publish
+    stat=$?; publish
   else
     IMG_TAG=$IMG_TAG REGISTRY=$REGISTRY docker-compose -f docker-compose.yaml up
 fi
